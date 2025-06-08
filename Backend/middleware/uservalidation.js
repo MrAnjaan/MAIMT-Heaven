4ab8dcdb1fdbd1716cacf12ba1ed444667a847e6
sagardalak <sagardalak11@gmail.com>
2025-06-08 17:06:03 +0530

    valdiation done

--- Backend/middleware/uservalidation.js
+++ Backend/middleware/uservalidation.js
@@ -18,11 +18,11 @@ next();
 
 const signupvalidation=(req,res,next)=>{
     const schema=joi.object({
-      
 
+        name:joi.string().email().required(),
         email:joi.string().email().required(),
-       student_id:joi.string().min(2).max(9).required(),
-       password: joi.string().min(5).required()
+        student_id:joi.string().min(2).max(9).required(),
+        password: joi.string().min(5).required()
 })
 const {error}=schema.validate(req.body)
 if(error)
